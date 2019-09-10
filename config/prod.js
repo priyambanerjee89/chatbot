8584f5d97382d878f6bef2bf416c36b10bfd51f2
Priyam Banerjee <priyam.banerjee@central1.com>
2019-09-10 11:45:10 -0700

    fix private key bug on heroku

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dislaogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 }
