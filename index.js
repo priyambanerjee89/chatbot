a4dc8dbb39d8248d8a34c6a6c00c929c76d139ee
Priyam Banerjee <priyam.banerjee@central1.com>
2019-09-10 12:01:10 -0700

    fix private key bug on heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 
 app.listen(PORT);
 
