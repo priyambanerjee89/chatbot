d17014be3e96ca643b859856a3c126491feb6dcb
Priyam Banerjee <priyam.banerjee@central1.com>
2019-09-13 10:21:29 -0700

    removed shop page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => (
     <nav>
         <div className="nav-wrapper">
-            <h6 style={{ marginLeft: '10px', display: 'inline-block' }}><Link to={'/'}>Frontend Forum Presentation</Link></h6>
+            <h6 style={{ marginLeft: '10px', display: 'inline-block' }}><Link to={'/'}>Created for Frontend Forum Presentation</Link></h6>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><Link to={'/shop'}>Buy Books</Link></li>
                 <li><Link to={'/about'}>About</Link></li>
             </ul>
         </div>
