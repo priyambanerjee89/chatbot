15e625e2731ed154be9af8dd164f0863efed340c
Priyam Banerjee <priyam.banerjee@central1.com>
2019-09-13 12:18:35 -0700

    removed unused page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import Landing from './pages/Landing';
 import About from './pages/About';
-import Shop from './shop/Shop';
 import Chatbot from './chatbot/Chatbot';
 
 const App = () => (
@@ -14,7 +13,6 @@ const App = () => (
                 <Header/>
                 <Route exact path="/" component={Landing} />
                 <Route exact path="/about" component={About} />
-                <Route exact path="/shop" component={Shop} />
                 <Chatbot/>
             </div>
         </BrowserRouter>
